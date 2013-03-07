50c1415c87d92e2bcb747b96fb13c69ac3a34b83
Hari Caushik <hcaushik@gmail.com>
2013-03-06 17:11:04 -0800

    measure number of mod operations per second

--- compute.c
+++ compute.c
@@ -4,12 +4,16 @@
 */
 
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
 	unsigned int num;
 	unsigned int i;
 	unsigned int sum = 0;
+	unsigned long nmod = 0;
+	int nsec;
+	unsigned int mod_per_sec;
 
 	if (!argc)
 	{
@@ -18,17 +22,24 @@ int main(int argc, char *argv[])
 	}
 	
 	num = atoi(argv[1]);
-
+	
+	clock_t start = clock(), diff;
 	for (i = 1; i <= num/2; i++)
 	{
 		if (!(num % i))
 			sum += i;
+		nmod++;
 	}
+	diff = clock() - start;
+	nsec = diff / CLOCKS_PER_SEC;
+	mod_per_sec = nmod / nsec;
 
 	if (sum == num)
 		printf("%u is a perfect number.\n", num);
 	else
 		
 		printf("%u is not a perfect number.\n", num);
+	
+	printf("Compute completed %d operations per second.\n", mod_per_sec);
 	return 0;
 }
